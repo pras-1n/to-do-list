ef14448ccb502fba0936010b3c297b107623bd5b
Prashant Guragai <prashantguragai1016@gmail.com>
2026-02-20 18:13:01 +0545

    removed the commented out codes

--- script.js
+++ script.js
@@ -45,62 +45,6 @@ function addTask(text = null, isCompleted = false) {
 		saveTasks();
 	}
 
-	// const li = document.createElement("li");
-	// if (isCompleted) li.classList.add("completed"); //if loaded from memory and was previously
-
-	// li.innerHTML = `
-	// 		<span>${taskText}</span>
-	// 		<button class="delete-btn">削除</button>
-	// 	`;
-
-	// li.querySelector("span").addEventListener("click", () => {
-	// 	li.classList.toggle("completed");
-	// 	saveTasks();
-	// });
-
-	// li.querySelector(".delete-btn").addEventListener("click", () => {
-	// 	li.remove();
-	// 	saveTasks();
-	// });
-
-	// // li.textContent = taskText; //replace this with the above line
-	// todoList.appendChild(li);
-	// if (!text) {
-	// 	input.value = "";
-	// 	input.focus();
-	// 	saveTasks();
-	// }
-
-	// } else {
-	// 	alert("新しいタスクを追加してください！");
-	// }
-
-	// span.addEventListener("dblclick", () => {
-	// 	const editInput = document.createElement("input");
-	// 	editInput.type = "text";
-	// 	editInput.value = span.textContent;
-	// 	editInput.classList.add("edit-input");
-
-	// 	span.replaceWith(editInput);
-	// 	editInput.focus();
-
-	// 	const saveEdit = () => {
-	// 		const newText = editInput.value.trim();
-	// 		if (newText !== "") {
-	// 			span.textContent = newText;
-	// 			saveTasks();
-	// 		}
-
-	// 		editInput.replaceWith(span);
-	// 	};
-
-	// 	editInput.addEventListener("keypress", (e) => {
-	// 		if (e.key === "Enter") saveEdit();
-	// 	});
-
-	// 	editInput.addEventListener("blur", saveEdit);
-	// });
-
 	span.addEventListener("dblclick", () => {
 		const editInput = document.createElement("input");
 		editInput.type = "text";
